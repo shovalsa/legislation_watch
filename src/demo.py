ee3844d15a33ca0ae96bda99d67bb4c9e439c8e8
Shoval Sadde <shovatz@gmail.com>
2023-04-28 00:29:03 +0300

    change radio to dropdown

--- src/demo.py
+++ src/demo.py
@@ -1,8 +1,9 @@
 import streamlit as st
 import requests
 import json
-from scraping import scrape_aspx, scrape, fetch
+# from scraping import scrape_aspx, scrape, fetch
 from summarize import load_model, summarize
+# from find_by_key import parse_key_words
 
 default_url = "https://www.gov.il/he/Departments/policies?OfficeId=e744bba9-d17e-429f-abc3-50f7a8a55667&blockCollector=true&limit=10&PmoMinistersComittee=a0d9709c-f07d-4b0e-8c48-0939643eb020&skip=0"
 with open('secrets/tokens.json', 'r') as f:
@@ -29,31 +30,33 @@ keywords = [
 ]
 
 scrape_url = False
-analyze_text = False
+search_kw = False
+summarize = False
 
 # Using "with" notation
 with st.sidebar:
     with st.form("url_form"):
-        url = st.text_input('אתר לחיפוש')
-        if not url:
-            url = st.radio(
-                "בחרו אתר לחיפוש טקסטים",
-                ("אתר א",
-                 "אתר ב",
-                 "אתר ג",
-                 "אתר ד")
-            )
-        submitted = st.form_submit_button("Submit")
-        if submitted:
-            scrape_url = True
+        url = st.sidebar.selectbox(
+            "בחרו אתר לחיפוש טקסטים",
+            ("choose option", "knesset", "gov.il", "load PDF/DOCX", "other URL")
+        )
+        if url != "choose option":
+            if url in ["load PDF/DOCX", "other URL"]:
+                st.write('insert url...')
+                url = st.text_input('insert url...')
+            submitted = st.form_submit_button("Submit")
+            if submitted:
+                scrape_url = True
 
     with st.form("text_form"):
         text_input = st.text_area('הכניסו טקסט')
 
-        submitted_text = st.form_submit_button("Submit")
+        submitted_text = st.form_submit_button("search keywords")
+        submit_to_summarize = st.form_submit_button("summarize_text")
         if submitted_text:
-            analyze_text = True
-
+            search_kw = True
+        if submit_to_summarize:
+            summarize = True
 
 
 def is_relevant(text, kwords):
@@ -73,18 +76,27 @@ model_name = "google/mt5-small"
 # model_name = "csebuetnlp/mT5_multilingual_XLSum"
 
 def main():
+
     st.title('זיהוי נושאים להטבים בהחלטות ממשלה')
-    # user_input = st.text_input('enter text', 'type here...')
-    # model, tokenizer = load_model(model_name)
     if scrape_url:
         st.write(url)
-    if analyze_text:
-        if text_input:
-            # summary = summarize(tokenizer, model, text=user_input)
-            st.subheader('summary')
-            # print(summary, type(summary))
+    if text_input:
+        if search_kw:
+            # keywords = parse_key_words().split("\n")
             is_relevant(text_input, keywords)
+        elif summarize:
+            st.subheader('summary')
+            summary = summarize(tokenizer, model, text=text_input)
+            st.write(summary)
+
 
 
 if __name__ == '__main__':
+    placeholder = st.empty()
+    placeholder.text("wait while model is loading...")
+    model, tokenizer = load_model(model_name)
+
+    placeholder.text('model loaded')
+
+
     main()
