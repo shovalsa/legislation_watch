7c4ef44f056942c0b8859f2212db1069849a6f9c
Maayan Zehavi <maayan@pagaya.com>
2023-04-28 00:49:25 +0300

    fix

--- src/common.py
+++ src/common.py
@@ -1,10 +1,6 @@
 from dataclasses import dataclass
 from datetime import datetime
 
-from src.scraping.file_parsing import read_pdf
-from src.scraping.knesset import parse_knesset
-from src.scraping.tools import scrape
-
 
 @dataclass
 class ScrapedData:
@@ -17,17 +13,3 @@ class ScrapedData:
 class AnalyzedData:
     data: str
     last_modified: datetime
-
-
-@dataclass
-class BECallable:
-    callable_method: callable
-    args: list
-
-
-BE_SCRAPE_MAPPING = {"knesset": BECallable(callable_method=parse_knesset, args=[]),
-                     "read from pdf": BECallable(callable_method=read_pdf, args=['pdf_path']),
-                     "read from website": BECallable(callable_method=scrape, args=['url'])
-                     }
-# BE_SCRAPE_MAPPING[called_method].callable_method(**kwargs)
-BE_ANALYZE_MAPPING = {}
